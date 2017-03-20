47d947cc2cc62dc7431b695510370a33037bf763
Robert <robertCamba@gmail.com>
2017-03-19 23:46:38 -0500

    generator for jm_dict, rename jukugo tests to kanji tests

---
+++ generators/jmdict_parser.py
@@ -0,0 +1,149 @@
+# coding=utf-8
+import os
+import json
+import xml.etree.ElementTree
+from collections import OrderedDict
+from kana_to_romaji.kana_to_romaji import kana_to_romaji
+
+
+PATH_TO_MODULE = os.path.dirname(__file__)
+JP_MAPPINGS_PATH = os.path.join(PATH_TO_MODULE, os.pardir, "jp_mappings")
+JM_DICT_FILE = os.path.join(PATH_TO_MODULE, os.pardir, os.pardir, "Downloads", "JMdict", "JMdict")
+
+
+def parse_out_verb(pos_type):
+    """
+    all ichidan verb grouped together
+    all godan verbs and "irregular nu verb" grouped together
+    archaic nidan and yodan are set to unclassified so they don't get added to dict...
+    verb types that can't get conjugated (aux, transitive, etc.) are grouped as general verb
+    suru and kuru verbs have their own group
+    else keep type: [u'counter', u'noun', u'particle', u'adverb', u'auxiliary', u'numeric', u'adjective', u'prefix',
+    u'conjunction', u'expression', u'interjection', u'suffix'])
+    :param pos_type:
+    :return:
+    """
+    if "Ichidan verb" in pos_type:
+        res = "ichidan verb"
+    elif ("Godan" in pos_type)or pos_type == "irregular nu verb":
+        res = "godan verb"
+    elif "archaic" in pos_type:
+        res = "unclassified"
+    elif ("intransitive verb" == pos_type or "auxiliary" == pos_type or "transitive verb" == pos_type or
+          pos_type == "auxiliary verb" or pos_type == "irregular ru verb, plain form ends with -ri" or
+          pos_type == "su verb - precursor to the modern suru" or
+          pos_type == "Godan verb with `u' ending (special class)"):
+        res = "verb"
+    elif "suru verb - irregular" == pos_type or "suru verb - special class" == pos_type:
+        res = "suru verb"
+    elif "Kuru verb - special class" == pos_type:
+        res = "kuru verb"
+    else:
+        res = pos_type
+    return res
+
+
+def strip_pos(pos_type):
+    res = pos_type
+    if "adjectival" in pos_type or "adjective" in pos_type:
+        res = "adjective"
+    elif "noun" in pos_type:
+        res = "noun"
+    elif "adverb" in pos_type:
+        res = "adverb"
+    elif "verb" in pos_type:
+        res = parse_out_verb(pos_type)
+    elif "numeric" in pos_type:
+        res = "numeric"
+    elif pos_type == "expressions (phrases, clauses, etc.)":
+        res = "expression"
+    elif pos_type == "interjection (kandoushi)":
+        res = "interjection"
+    return res
+
+
+def main():
+    """
+    iterate through each entry of JM_DICT
+    use first romaji reading found
+    if pos is unclassified or misc is archaism then do not include to dict
+    multiple entries can have the same kanji
+        only replace a kanji in dict if it has "ichi1/2" for ke_pri
+    """
+    auto_jm_dict = {}
+    root = xml.etree.ElementTree.parse(JM_DICT_FILE).getroot()
+    entries = root.findall("entry")
+
+    for e in entries:
+        first_romaji_reading = e.iterfind("r_ele").next().iterfind("reb").next().text
+        raw_first_pos = e.iterfind("sense").next().iterfind("pos").next().text
+        misc = [m.text for m in e.iterfind("sense").next().iterfind("misc")]
+        stripped_first_pos = strip_pos(raw_first_pos)
+        if stripped_first_pos != "unclassified" and "archaism" not in misc:
+
+            for k_ele in e.iterfind("k_ele"):
+                for k_ in k_ele.iterfind("keb"):
+                    if k_.text in auto_jm_dict:
+                        try:
+                            ke_pris = [kp.text for kp in k_ele.iterfind("ke_pri")]
+                            if ("ichi1" in ke_pris or "ichi2" in ke_pris) and \
+                                    auto_jm_dict[k_.text]["ichi"] is False:
+                                auto_jm_dict[k_.text] = {
+                                    "romaji": kana_to_romaji(first_romaji_reading),
+                                    "w_type": stripped_first_pos,
+                                    "ichi": True
+                                }
+                        except IndexError:
+                            if k_.text in auto_jm_dict:
+                                del auto_jm_dict[k_.text]
+                                print k_.text
+
+                        except StopIteration:
+                            pass
+
+                    else:
+                        try:
+                            is_ichi = "ichi" in k_ele.iterfind("ke_pri").next().text
+                        except StopIteration:
+                            is_ichi = False
+                        try:
+                            auto_jm_dict[k_.text] = {
+                                    "romaji": kana_to_romaji(first_romaji_reading),
+                                    "w_type": stripped_first_pos,
+                                    "ichi": is_ichi
+                                }
+                        except IndexError:
+                            if k_.text in auto_jm_dict:
+                                del auto_jm_dict[k_.text]
+                                print k_.text
+
+    del auto_jm_dict[u"々"]
+
+    return auto_jm_dict
+
+
+def load_kanji_mappings_dict_no_jm_autod():
+    kanji_romaji_mapping = {}
+    for f in os.listdir(JP_MAPPINGS_PATH):
+        if os.path.splitext(f)[1] == ".json" and "kanji" in f and "jm_dict_autod_kanji" not in f:
+            with open(os.path.join(JP_MAPPINGS_PATH, f)) as data_file:
+                kanji_romaji_mapping.update(json.load(data_file))
+    return kanji_romaji_mapping
+
+
+if __name__ == "__main__":
+    knj = load_kanji_mappings_dict_no_jm_autod()
+
+    ajd = main()
+
+    for k in ajd.keys():
+        if k in knj:
+            del ajd[k]
+        else:
+            del ajd[k]["ichi"]
+
+    ordered_kanji_dict = OrderedDict(sorted(ajd.items(),
+                                            key=lambda item: (len(item[0]), item[0]), reverse=True))
+    okd_str = json.dumps(ordered_kanji_dict, indent=2, ensure_ascii=False, encoding="utf-8", separators=(',', ': '))
+    with open("C:\\Users\\Kevin\\kana_to_romaji\\jp_mappings\\jm_dict_autod_kanji.json", 'w') as writer:
+        writer.write(str(okd_str.encode("utf-8")))
---
+++ tests/test_kanji.py
@@ -0,0 +1,151 @@
+# coding=utf-8
+import unittest
+from kana_to_romaji.kana_to_romaji import kana_to_romaji
+
+
+class TestKanji(unittest.TestCase):
+    def setUp(self):
+        print "\nStarting " + self.__module__ + ": " + self._testMethodName
+
+    # test_日
+    def test_u65E5(self):
+        self.assertEqual(kana_to_romaji(u"曜日"), "youbi")
+        self.assertEqual(kana_to_romaji(u"今日"), "kyou")
+        self.assertEqual(kana_to_romaji(u"人日"), "ninnichi")
+        self.assertEqual(kana_to_romaji(u"日記"), "nikki")
+        self.assertEqual(kana_to_romaji(u"日給"), "nikkyuu")
+        self.assertEqual(kana_to_romaji(u"日系"), "nikkei")
+        self.assertEqual(kana_to_romaji(u"日月"), "jitsugetsu")
+        self.assertEqual(kana_to_romaji(u"日銀"), "nichigin")
+        self.assertEqual(kana_to_romaji(u"日光"), "nikkou")
+        self.assertEqual(kana_to_romaji(u"日照"), "nisshou")
+        self.assertEqual(kana_to_romaji(u"日章旗"), "nisshouki")
+        self.assertEqual(kana_to_romaji(u"日進月歩"), "nisshingeppo")
+        self.assertEqual(kana_to_romaji(u"日中"), "nitchuu")
+        self.assertEqual(kana_to_romaji(u"日常"), "nichijou")
+        self.assertEqual(kana_to_romaji(u"日常茶飯事"), "nichijousahanji")
+        self.assertEqual(kana_to_romaji(u"日程"), "nittei")
+        self.assertEqual(kana_to_romaji(u"日々"), "hibi")
+        self.assertEqual(kana_to_romaji(u"日暮れ"), "higure")
+        self.assertEqual(kana_to_romaji(u"日没"), "nichibotsu")
+        self.assertEqual(kana_to_romaji(u"日本"), "nihon")
+        self.assertEqual(kana_to_romaji(u"日夜"), "nichiya")
+        self.assertEqual(kana_to_romaji(u"日用"), "nichiyou")
+        self.assertEqual(kana_to_romaji(u"日曜"), "nichiyou")
+        self.assertEqual(kana_to_romaji(u"日曜日"), "nichiyoubi")
+        self.assertEqual(kana_to_romaji(u"日輪"), "nichirin")
+        self.assertEqual(kana_to_romaji(u"日帰り"), "higaeri")
+        self.assertEqual(kana_to_romaji(u"日陰"), "hikage")
+        self.assertEqual(kana_to_romaji(u"日影"), "hikage")
+        self.assertEqual(kana_to_romaji(u"日傘"), "higasa")
+        self.assertEqual(kana_to_romaji(u"日柄"), "higara")
+        self.assertEqual(kana_to_romaji(u"日時計"), "hidokei")
+        self.assertEqual(kana_to_romaji(u"日付"), "hizuke")
+        self.assertEqual(kana_to_romaji(u"日付変更線"), "hizukehenkousen")
+        self.assertEqual(kana_to_romaji(u"日向"), "hinata")
+        self.assertEqual(kana_to_romaji(u"日読み"), "hiyomi")
+        self.assertEqual(kana_to_romaji(u"日和"), "hiyori")
+        self.assertEqual(kana_to_romaji(u"日和見"), "hiyorimi")
+        self.assertEqual(kana_to_romaji(u"毎日"), "mainichi")
+        self.assertEqual(kana_to_romaji(u"先日"), "senjitsu")
+        self.assertEqual(kana_to_romaji(u"昨日"), "kinou")
+        self.assertEqual(kana_to_romaji(u"明日"), "ashita")
+        self.assertEqual(kana_to_romaji(u"在日"), "zainichi")
+        self.assertEqual(kana_to_romaji(u"尽日"), "jinjitsu")
+
+    def test_arbit_common(self):
+        test_and_expected = {
+            u"私": "watashi",
+            u"僕": "boku",
+            u"君": "kimi"
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_kanji_iteration_mark(self):
+        # regular
+        self.assertEqual(kana_to_romaji(u"若"), "waka")
+        self.assertEqual(kana_to_romaji(u"若々しい"), "wakawakashii")
+
+        x = kana_to_romaji(u"在")
+        self.assertEqual(kana_to_romaji(u"在"), x)
+        self.assertEqual(kana_to_romaji(u"在々"), x * 2)
+
+        # irregular that has to be listed in dict
+        self.assertEqual(kana_to_romaji(u"精々"), "seizei")
+        self.assertEqual(kana_to_romaji(u"日々"), "hibi")
+
+    def test_no_particle(self):
+        test_and_expected = {
+            u"災厄の時代": u"saiyaku no jidai",  # noun followed by KanjiBlock/noun
+            u"私のパーティー": u"watashi no paatii",  # type change between no character  (hira no kata)
+            u"さいやくのじだい": u"saiyakunojidai"  # no KanjiBlocks and no change in type
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_wa_particle(self):
+        test_and_expected = {
+            u"私は嬉": u"watashi wa ureshii",  # noun followed by KanjiBlock/adjective
+            u"わたしはロバート": u"watashi wa robaato",  # type change between ha character (hira ha kata)
+            u"わたしはうれしい": u"watashihaureshii"  # no KanjiBlocks and no change in type
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_he_particle(self):
+        test_and_expected = {
+            u"部屋へ帰る": u"heya e kaeru",  # noun followed by KanjiBlock/adjective
+            u"アパートへくる": u"apaato e kuru",  # type change between he character (kata he hira)
+            u"へやへかえる": u"heyahekaeru"  # no KanjiBlocks and no change in type
+        }
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_to_particle(self):
+        test_and_expected = {
+            u"私と猫-ちゃん": u"watashi to neko-chan",  # noun followed by KanjiBlock
+            u"運命という": u"unmei to iu",  # type change between to character (kanji to hira)
+            u"わたしとねこ-ちゃん": u"watashitoneko-chan"  # no KanjiBlocks and no change in type
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_ni_particle(self):
+        test_and_expected = {
+            u"友達に会いました": u"tomodachi ni aimashita",  # noun followed by KanjiBlock/verb
+            u"ともだちにあいました": u"tomodachiniaimashita"  # no KanjiBlocks and no change in type
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+    def test_type_changes(self):
+        self.assertEqual(kana_to_romaji(u"ごと."), "goto.")
+
+    def test_kanjiblock_curr_chars_rep(self):
+        self.assertEqual(kana_to_romaji(u"食べる存在"), "taberu sonzai")
+
+    def test_particle_followed_by_particle(self):
+        test_and_expected = {
+            u"アメリカでは何語が話されていますか": "amerika DE WA nanigo ga hanashisareteimasuka".lower(),
+            u"車には一人分の空きがあった": "kuruma NI WA hitoribun no aki ga atta".lower(),
+            u"ボタンとはなんですか": "botan TO WA nandesuka".lower(),
+
+            u"僕にも責任があるんだ": "boku NI MO sekinin ga arunda".lower(),
+            u"どんな子供でもそのくらい答えられる": "donnakodomo DE MO sonokuraikotae rareru".lower(),
+
+            u"部屋にははいります": "heya NI WA HAirimasu".lower(),
+            u"私にはにだいめ": "watashi NI WA NIdaime".lower()
+        }
+
+        for key in test_and_expected.keys():
+            self.assertEqual(kana_to_romaji(key), test_and_expected[key])
+
+
+if __name__ == "__main__":
+    unittest.main()
