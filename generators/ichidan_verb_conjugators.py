8ae1d48f203144328f778137955355482cbf34e4
Robert <robertCamba@gmail.com>
2017-05-05 21:17:28 -0500

    ichidan verb conjugator + mappings

---
+++ generators/ichidan_verb_conjugators.py
@@ -0,0 +1,156 @@
+# coding=utf-8
+import os
+import json
+from kana_to_romaji.kana_to_romaji import kana_to_romaji
+
+PATH_TO_MODULE = os.path.dirname(__file__)
+JP_MAPPINGS_PATH = os.path.join(PATH_TO_MODULE, os.pardir, "jp_mappings")
+
+
+def set_global_ichidan(isk, isr):
+    global ichidan_stem_kana
+    global ichidan_stem_romaji
+    ichidan_stem_kana = isk
+    ichidan_stem_romaji = isr
+
+
+def conjugate_ichidan_polite_present_affirmative():
+    conjugated_kana = ichidan_stem_kana + u"ます"
+    conjugated_romaji = ichidan_stem_romaji + "masu"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_negative():
+    conjugated_kana = ichidan_stem_kana + u"ない"
+    conjugated_romaji = ichidan_stem_romaji + "nai"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_present_negative():
+    conjugated_kana = ichidan_stem_kana + u"ません"
+    conjugated_romaji = ichidan_stem_romaji + "masen"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_past():
+    conjugated_kana = ichidan_stem_kana + u"た"
+    conjugated_romaji = ichidan_stem_romaji + "ta"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_past():
+    conjugated_kana = ichidan_stem_kana + u"ました"
+    conjugated_romaji = ichidan_stem_romaji + "mashita"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_past_negative():
+    conjugated_kana = ichidan_stem_kana + u"なかった"
+    conjugated_romaji = ichidan_stem_romaji + "nakatta"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_past_negative():
+    conjugated_kana = ichidan_stem_kana + u"ませんでした"
+    conjugated_romaji = ichidan_stem_romaji + "masen deshita"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_te_form():
+    conjugated_kana = ichidan_stem_kana + u"て"
+    conjugated_romaji = ichidan_stem_romaji + "te"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_te_form():
+    conjugated_kana = ichidan_stem_kana + u"まして"
+    conjugated_romaji = ichidan_stem_romaji + "mashite"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_te_form_negative():
+    conjugated_kana = ichidan_stem_kana + u"ないで"
+    conjugated_romaji = ichidan_stem_romaji + "naide"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_te_form_negative():
+    conjugated_kana = ichidan_stem_kana + u"ませんで"
+    conjugated_romaji = ichidan_stem_romaji + "masende"
+    return conjugated_kana, conjugated_romaji
+
+# plain/polite conditional(negative) are past + ra
+
+
+def conjugate_ichidan_plain_volitional():
+    conjugated_kana = ichidan_stem_kana + u"よう"
+    conjugated_romaji = ichidan_stem_romaji + "you"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_volitional():
+    conjugated_kana = ichidan_stem_kana + u"ましょう"
+    conjugated_romaji = ichidan_stem_romaji + "mashou"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_plain_imperative():
+    conjugated_kana = ichidan_stem_kana + u"ろ"
+    conjugated_romaji = ichidan_stem_romaji + "ro"
+    return conjugated_kana, conjugated_romaji
+
+
+# plain imperative negative is dictionary form + na; no need to conjugate
+
+def conjugate_ichidan_polite_imperative():
+    conjugated_kana = ichidan_stem_kana + u"なさい"
+    conjugated_romaji = ichidan_stem_romaji + "nasai"
+    return conjugated_kana, conjugated_romaji
+
+
+def conjugate_ichidan_polite_imperative_negative():
+    conjugated_kana = ichidan_stem_kana + u"なさるな"
+    conjugated_romaji = ichidan_stem_romaji + "nasaruna"
+    return conjugated_kana, conjugated_romaji
+
+
+if __name__ == "__main__":
+    f = os.path.join(JP_MAPPINGS_PATH, "jm_dict_autod_kanji.json")
+    with open(os.path.join(f)) as data_file:
+        jm_dict = json.load(data_file)
+
+    conjugator_funcs = [
+        conjugate_ichidan_polite_present_affirmative,
+        conjugate_ichidan_plain_negative,
+        conjugate_ichidan_polite_present_negative,
+        conjugate_ichidan_plain_past,
+        conjugate_ichidan_polite_past,
+        conjugate_ichidan_plain_past_negative,
+        conjugate_ichidan_polite_past_negative,
+        conjugate_ichidan_plain_te_form,
+        conjugate_ichidan_polite_te_form,
+        conjugate_ichidan_plain_te_form_negative,
+        conjugate_ichidan_polite_te_form_negative,
+        conjugate_ichidan_plain_volitional,
+        conjugate_ichidan_polite_volitional,
+        conjugate_ichidan_plain_imperative,
+        conjugate_ichidan_polite_imperative,
+        conjugate_ichidan_polite_imperative_negative
+    ]
+
+    conjugated_mappings = {}
+    for k in jm_dict.keys():
+        if jm_dict[k]["w_type"] == "ichidan verb":
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            for c_func in conjugator_funcs:
+                ck, cr = c_func()
+                conjugated_mappings[ck] = {
+                    "romaji": cr,
+                    "w_type": "conjugated ichidan verb"
+                }
+
+    okd_str = json.dumps(conjugated_mappings, indent=2, ensure_ascii=False, encoding="utf-8",
+                         separators=(',', ': '))
+
+    with open(os.path.join(JP_MAPPINGS_PATH, "conjugated_ichidan_kanji.json"), 'w') as writer:
+        writer.write(str(okd_str.encode("utf-8")))
---
+++ tests/test_ichidan_verb_conjugators.py
@@ -0,0 +1,211 @@
+# coding=utf-8
+import unittest
+from kana_to_romaji.kana_to_romaji import kana_to_romaji
+from generators.ichidan_verb_conjugators import conjugate_ichidan_plain_te_form, conjugate_ichidan_plain_te_form_negative, \
+    conjugate_ichidan_plain_volitional, conjugate_ichidan_polite_volitional, conjugate_ichidan_plain_imperative, \
+    conjugate_ichidan_polite_imperative, conjugate_ichidan_polite_present_affirmative, conjugate_ichidan_plain_negative, \
+    conjugate_ichidan_polite_present_negative, conjugate_ichidan_polite_past, conjugate_ichidan_plain_past, \
+    conjugate_ichidan_polite_past_negative, conjugate_ichidan_plain_past_negative, \
+    conjugate_ichidan_polite_imperative_negative, conjugate_ichidan_polite_te_form, \
+    conjugate_ichidan_polite_te_form_negative, set_global_ichidan
+
+
+class TestGodanVerbConjugators(unittest.TestCase):
+    def setUp(self):
+        print "\nStarting " + self.__module__ + ": " + self._testMethodName
+
+    def test_polite_present_affirmative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ます", "nemasu"),
+            u"出来る": (u"出来ます", "dekimasu"),
+            u"見つける": (u"見つけます", "mitsukemasu")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_present_affirmative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ない", "nenai"),
+            u"出来る": (u"出来ない", "dekinai"),
+            u"見つける": (u"見つけない", "mitsukenai")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_present_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ません", "nemasen"),
+            u"出来る": (u"出来ません", "dekimasen"),
+            u"見つける": (u"見つけません", "mitsukemasen")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_present_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_past(self):
+        ichidan_expected = {
+            u"寝る": (u"寝た", "neta"),
+            u"出来る": (u"出来た", "dekita"),
+            u"見つける": (u"見つけた", "mitsuketa")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_past()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_past(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ました", "nemashita"),
+            u"出来る": (u"出来ました", "dekimashita"),
+            u"見つける": (u"見つけました", "mitsukemashita")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_past()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_past_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝なかった", "nenakatta"),
+            u"出来る": (u"出来なかった", "dekinakatta"),
+            u"見つける": (u"見つけなかった", "mitsukenakatta")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_past_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_past_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ませんでした", "nemasen deshita"),
+            u"出来る": (u"出来ませんでした", "dekimasen deshita"),
+            u"見つける": (u"見つけませんでした", "mitsukemasen deshita")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_past_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_te_form(self):
+        ichidan_expected = {
+            u"寝る": (u"寝て", "nete"),
+            u"出来る": (u"出来て", "dekite"),
+            u"見つける": (u"見つけて", "mitsukete")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_te_form()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_te_form(self):
+        ichidan_expected = {
+            u"寝る": (u"寝まして", "nemashite"),
+            u"出来る": (u"出来まして", "dekimashite"),
+            u"見つける": (u"見つけまして", "mitsukemashite")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_te_form()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_te_form_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ないで", "nenaide"),
+            u"出来る": (u"出来ないで", "dekinaide"),
+            u"見つける": (u"見つけないで", "mitsukenaide")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_te_form_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_te_form_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ませんで", "nemasende"),
+            u"出来る": (u"出来ませんで", "dekimasende"),
+            u"見つける": (u"見つけませんで", "mitsukemasende")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_te_form_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_volitional(self):
+        ichidan_expected = {
+            u"寝る": (u"寝よう", "neyou"),
+            u"出来る": (u"出来よう", "dekiyou"),
+            u"見つける": (u"見つけよう", "mitsukeyou")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_volitional()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_volitional(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ましょう", "nemashou"),
+            u"出来る": (u"出来ましょう", "dekimashou"),
+            u"見つける": (u"見つけましょう", "mitsukemashou")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_volitional()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_plain_imperative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝ろ", "nero"),
+            u"出来る": (u"出来ろ", "dekiro"),
+            u"見つける": (u"見つけろ", "mitsukero")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_plain_imperative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_imperative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝なさい", "nenasai"),
+            u"出来る": (u"出来なさい", "dekinasai"),
+            u"見つける": (u"見つけなさい", "mitsukenasai")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_imperative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+    def test_polite_imperative_negative(self):
+        ichidan_expected = {
+            u"寝る": (u"寝なさるな", "nenasaruna"),
+            u"出来る": (u"出来なさるな", "dekinasaruna"),
+            u"見つける": (u"見つけなさるな", "mitsukenasaruna")
+        }
+
+        for k in ichidan_expected.keys():
+            set_global_ichidan(k[:-1], kana_to_romaji(k)[:-2])
+            ck, cr = conjugate_ichidan_polite_imperative_negative()
+            self.assertEqual(ichidan_expected[k], (ck, cr))
+
+
+if __name__ == "__main__":
+    unittest.main()
