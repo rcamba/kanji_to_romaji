ca376416fe2e4d8b2da77e579cd27288055ad2df
Robert <robertCamba@gmail.com>
2017-05-06 00:35:26 -0500

    add models and jp_mappings packages to setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ PATH_TO_MODULE = os.path.dirname(__file__)
 setup(
     name='Kana_to_Romaji',
     version='1.0',
-    packages=['kana_to_romaji'],
+    packages=['kana_to_romaji', 'models', 'jp_mappings'],
+    package_data={"jp_mappings": ["./*.json"]},
     url='https://github.com/rcamba/kana_to_romaji',
     license='',
     author='Robert Camba',
     author_email='rcamba@users.noreply.github.com',
     description='Translate Kana to Romaji with some weak support for some Kanji.',
-    data_files=[("jp_mappings", [os.path.join("jp_mappings", f)
-                                 for f in os.listdir(os.path.join(PATH_TO_MODULE, "jp_mappings"))])],
     extras_require={
         'fasterjson': ["simplejson>=3.10.0"],
     }
